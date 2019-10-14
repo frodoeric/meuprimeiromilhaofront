5c722f94abaaec48f763beb2c9f7a3c1d9a807ab
ericcostatotvs <ericcostatotvs@users.noreply.github.com>
2019-10-14 13:22:18 -0300

    criado métodos de acesso ao backend

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const URL_GERAMILHO = "https://localhost:44351/api/GeraMilho";
+const URL_DEPOSITO = "https://localhost:44351/api/Deposito";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getGerarMilho(){
+    return this.http.get(URL_GERAMILHO);
+  }
+
+  getDeposito(){
+    return this.http.get(URL_DEPOSITO);
+  }
 }
