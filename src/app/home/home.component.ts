49be4441c787e482d4c5fe54ab280f529a972b2a
ericcostatotvs <ericcostatotvs@users.noreply.github.com>
2019-10-14 13:48:23 -0300

    inclusão layout padrão

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  title = "Home";
+  soma: number = 0;
+
+  constructor(private homeServe: HomeService) { }
 
   ngOnInit() {
+  }
+
+  somar(){
+    this.soma ++;
   }
 
 }
